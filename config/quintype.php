f7ade3f452c0f8bde12a87888832be9aae947d75
Tejas Dinkar <tejas@gja.in>
2016-11-27 10:46:09 +0530

    Update quintype.php

--- config/quintype.php
+++ config/quintype.php
@@ -3,7 +3,7 @@
 return [
     "publisher-name" => "hamiltonelectors",
     "asset-host" => "",
-    "api-host" => "http://hamiltonelectors.staging.quintype.io/",
+    "api-host" => "http://hamiltonelectors.quintype.io/",
     "image-cdn" => "//quintype-01.imgix.net",
     "title" => "Hamilton Electors"
 ];
